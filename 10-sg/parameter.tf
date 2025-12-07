87cec53a3130e2f5591223eb8442b3d50e66a5dd
L V Ramana <lellalvramana@gmail.com>
2025-12-07 11:23:41 +0530

    updated all sg

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "front_end" {
-    name = "/${var.project}/${var.environment}/frontend"
+    name = "/${var.project}/${var.environment}/frontend_sg_id"
     type = "String"
     value = module.front_end_sg.sg_id
 }
@@ -23,7 +23,7 @@ resource "aws_ssm_parameter" "vpn_sg_id"{
 }
 
 resource "aws_ssm_parameter""mongodb_sg_id"{
-    name = "/${var.project}/${var.environment}/mongod_sg"
+    name = "/${var.project}/${var.environment}/mongod_sg_id"
     type = "String"
     value = module.mongodb_sg.sg_id
 }
@@ -53,7 +53,7 @@ resource "aws_ssm_parameter""catalogue_sg_id"{
 }
 
 resource "aws_ssm_parameter""frontend-alb-sg"{
-    name = "/${var.project}/${var.environment}/frontend_alb_sg"
+    name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
     type = "String"
     value = module.frontend_alb_sg.sg_id
 }
@@ -62,4 +62,19 @@ resource "aws_ssm_parameter""user_sg_id"{
     name = "/${var.project}/${var.environment}/user_sg_id"
     type = "String"
     value = module.user_sg.sg_id
+}
+resource "aws_ssm_parameter""cart_sg_id"{
+    name = "/${var.project}/${var.environment}/cart_sg_id"
+    type = "String"
+    value = module.cart_sg.sg_id
+}
+resource "aws_ssm_parameter""shipping_sg_id"{
+    name = "/${var.project}/${var.environment}/shipping_sg_id"
+    type = "String"
+    value = module.shipping_sg.sg_id
+}
+resource "aws_ssm_parameter""payment_sg_id"{
+    name = "/${var.project}/${var.environment}/payment_sg_id"
+    type = "String"
+    value = module.payment_sg.sg_id
 }
